dcd4b3af47a055854687e4710c2463fcededd980
Anand prabhakar <anandprabhakar0507@gmail.com>
2018-10-28 10:10:28 -0700

    Create Div.py

---
+++ Div.py
@@ -0,0 +1,13 @@
+# This script is for division
+
+x = 7
+y = 3
+
+# divide them
+division= float(x) / float(y)
+
+#get quotient
+quotient= float(x) % float(y)
+
+# Display the Result
+print('The quotient of {0} and {1} is {2} and the remainder is {3} '.format(x, y, division, quotient))
